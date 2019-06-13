a2efec72a7a7eb2a2689ec4cc8ea2dc8cfc198a5
yudawardana <yuda.wardana40@gmail.com>
2019-06-13 21:15:49 +0700

    news and oauth

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,35 @@
 const route = require('express').Router()
 const { ControllerUser, ControllerTextToSpeech } = require('../controllers')
 const { authenticate } = require('../middlewares/auth')
+const news = require('../routes/newsApiRouter')
+const logingoogle = require('../routes/loginGoogle')
+const {OAuth2Client} = require('google-auth-library');
 
 route.get('/', (req, res) => {res.status(200).json({message: 'Connect'})})
 route.get('/authenticate',authenticate,(req,res)=>{ res.status(200).json({}) })
 route.post('/register', ControllerUser.create)
 route.post('/login', ControllerUser.login)
 route.post('/textToSpeech', ControllerTextToSpeech.textToSpeech)
+
+route.post('/logingoogle',(req,res)=> {
+    console.log ('masuk logingoogle')
+    const client = new OAuth2Client("132166001773-nljl5umc0vmdlvodji12sfl1eqk051q7.apps.googleusercontent.com");
+        
+        client.verifyIdToken({
+            idToken : req.body.id_token,
+        })
+            .then(ticket=> {
+                console.log('ticket')
+                const payload = ticket.getPayload();
+                console.log (payload)
+            })
+        .catch(err =>{
+            console.log('masuk error')
+            console.log(err)
+        });
+})
+route.use('/news', news)
+
 
 route.use('/*', (req, res) => res.status(404).json({error: 'Not Found :('}))
 
